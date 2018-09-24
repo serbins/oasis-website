ec8d04d86d851425e3441d6368a824b77a14b1e5
Steve <steveserbins@gmail.com>
2018-09-24 14:27:24 -0600

    Updated by adding a quuote app

--- goodies/js/app.js
+++ goodies/js/app.js
@@ -50,31 +50,36 @@ $('a[href*="#"]')
 //     $('#quickQuote').modal();
 // }, 5000);
 
-  // Quote App 
-  $("#generate").click(function() {
-    let sqFt = document.getElementById("sqFt").value;
+
+
+    $("#generate").click( function() {
+
+    sqFt = $("#sqFt").val();
     
-     if (sqFt < ' ') {
-      document.getElementById("result").innerHTML = 'Please enter a number greater than 0';
+
+    if (sqFt < ' ') {
+    $("#result").html('Please enter a number greater than 0');
     } else if (sqFt < 1000 ) {
-      document.getElementById("result").innerHTML = 'Your estimated service quote is less than $195 and will take about 1 - 1.5 hours to be completed';
+    $("#result").html('Your estimated service quote is less than $195 and will take about 1 - 1.5 hours to be completed');
     } else if (sqFt == 1000 || sqFt <= 1200 ) {
-      document.getElementById("result").innerHTML = 'Your estimated service quote is $195 - $275+ and will take about 1.5 - 2 hours to be completed';
+      $("#result").html('Your estimated service quote is $195 - $275+ and will take about 1.5 - 2 hours to be completed');
     } else if (sqFt == 1201 || sqFt <= 2000) {
-      document.getElementById("result").innerHTML = 'Your estimated service quote is $300 - $400+ and will take about 2.5 - 3 hours to be completed';
+      $("#result").html('Your estimated service quote is $300 - $400+ and will take about 2.5 - 3 hours to be completed');
     } else if (sqFt == 2001 || sqFt <= 3000) {
-      document.getElementById("result").innerHTML = 'Your estimated service quote is $400 - $555+ and will take about 3 - 3.5 hours to be completed';
+      $("#result").html('Your estimated service quote is $400 - $555+ and will take about 3 - 3.5 hours to be completed');
     } else if (sqFt == 3001 || sqFt <= 4500) {
-      document.getElementById("result").innerHTML = 'Your estimated service quote is $600 - $800+ and will take about 4 - 5 hours to be completed';
+      $("#result").html('Your estimated service quote is $600 - $800+ and will take about 4 - 5 hours to be completed');
     } else if (sqFt == 4501 || sqFt <= 5500) {
-      document.getElementById("result").innerHTML = 'Your estimated service quote is $950 - $1,400+ and will take about 5 - 7 hours to be completed';
+      $("#result").html('Your estimated service quote is $950 - $1,400+ and will take about 5 - 7 hours to be completed');
     } else if (sqFt >= 5501 ) {
-      document.getElementById("result").innerHTML = 'Your estimated service quote is $1,400+ and will take about 5 - 7 hours to be completed';
+      $("#result").html('Your estimated service quote is $1,400+ and will take about 5 - 7 hours to be completed');
     } else {
-      document.getElementById("result").innerHTML = 'Looks like somethign went wrong! Be sure to use only numbers in the input field';
+      $("#result").html('Looks like something went wrong! Be sure to use only numbers in the input field');
     }
-    
-    });
-  
-});
+
+    }); //function of quote app
+ 
+
+}); // Doc Ready 
+
 
